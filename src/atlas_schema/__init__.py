a6ccdb728809591180b37739aceb16ba6afde43b
Giordon Stark <kratsg@gmail.com>
2025-01-14 10:59:30 -0800

    ignore coffea vector warnings

--- src/atlas_schema/__init__.py
+++ src/atlas_schema/__init__.py
@@ -6,7 +6,11 @@ atlas_schema: Collection of utilities and helper functions for HEP ATLAS analyse
 
 from __future__ import annotations
 
+import warnings
+
 from atlas_schema._version import version as __version__
 from atlas_schema.enums import ParticleOrigin, PhotonID
+
+warnings.filterwarnings("ignore", module="coffea.*")
 
 __all__ = ["ParticleOrigin", "PhotonID", "__version__"]
