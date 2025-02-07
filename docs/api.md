72c66a1b503c55763cb54d5e7160fb1f937b77e8
Giordon Stark <kratsg@gmail.com>
2025-02-06 20:57:53 -0800

    add in more functionality, get toctree improved?

---
+++ docs/api.md
@@ -0,0 +1,16 @@
+# Python API
+
+## Top-Level
+
+```{currentmodule}
+atlas_schema
+```
+
+```{autosummary}
+:toctree: _generated/
+:nosignatures:
+
+ParticleOrigin
+PhotonID
+~atlas_schema.schema.NtupleSchema
+```
--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
 import importlib.metadata
+from typing import Any
+
+from intersphinx_registry import get_intersphinx_mapping
 
 project = "atlas-schema"
 copyright = "2024, Giordon Stark"
@@ -10,13 +13,33 @@ version = release = importlib.metadata.version("atlas-schema")
 extensions = [
     "myst_parser",
     "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.ifconfig",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
     "sphinx_autodoc_typehints",
+    "sphinx_copybutton",
+    "sphinx_click.ext",
+    "sphinx_issues",
     "sphinx_copybutton",
 ]
 
+# GitHub repo
+issues_github_path = "scipp-atlas/atlas-schema"
+
+# Generate the API documentation when building
+autosummary_generate = True
+numpydoc_show_class_members = False
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
 source_suffix = [".rst", ".md"]
 exclude_patterns = [
     "_build",
@@ -29,13 +52,33 @@ exclude_patterns = [
 
 html_theme = "furo"
 
+html_theme_options: dict[str, Any] = {
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/scipp-atlas/atlas-schema",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
+    "source_repository": "https://github.com/scipp-atlas/atlas-schema",
+    "source_branch": "main",
+    "source_directory": "docs/",
+}
+
 myst_enable_extensions = [
     "colon_fence",
 ]
 
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3", None),
-}
+intersphinx_mapping = get_intersphinx_mapping(
+    packages={
+        "python",
+    }
+)
 
 nitpick_ignore = [
     ("py:class", "_io.StringIO"),
@@ -43,3 +86,8 @@ nitpick_ignore = [
 ]
 
 always_document_param_types = True
+
+# sphinx-copybutton configuration
+copybutton_prompt_text = r">>> |\.\.\. |\$ "
+copybutton_prompt_is_regexp = True
+copybutton_here_doc_delimiter = "EOF"
