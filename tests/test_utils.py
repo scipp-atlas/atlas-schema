be40ada81118107dd7246ae090f45295680caec0
Giordon Stark <kratsg@gmail.com>
2025-02-13 10:29:23 -0800

    add test for another dimension to be sure

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,3 +22,14 @@ def test_isin_diffaxis():
     result = ak.Array([[True, False, False], [False], [False, False, False], [True]])
     assert ak.all(ats.isin(array, test) == result)
     assert ak.all(ats.isin(array_dask, test).compute() == result)
+
+
+def test_isin_moredimensions():
+    array = ak.Array([[[1], [2], [3]], [[4]], [[5], [6], [7]], [[1]]])
+    array_dask = dak.from_awkward(array, npartitions=2)
+    test = ak.Array([1, 2, 7])
+    result = ak.Array(
+        [[[True], [True], [False]], [[False]], [[False], [False], [True]], [[True]]]
+    )
+    assert ak.all(ats.isin(array, test) == result)
+    assert ak.all(ats.isin(array_dask, test).compute() == result)
