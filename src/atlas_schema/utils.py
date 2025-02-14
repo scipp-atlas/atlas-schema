2a1831a7b5fdbd0606922354ba2e23e176693e04
Giordon Stark <kratsg@gmail.com>
2025-02-13 16:25:26 -0800

    rename haystack/needles to numpy's api

--- src/atlas_schema/utils.py
+++ src/atlas_schema/utils.py
@@ -10,23 +10,23 @@ Array = TypeVar("Array", bound=Union[dak.Array, ak.Array])
 _E = TypeVar("_E", bound=Enum)
 
 
-def isin(haystack: Array, needles: dak.Array | ak.Array, axis: int = -1) -> Array:
+def isin(element: Array, test_elements: dak.Array | ak.Array, axis: int = -1) -> Array:
     """
-    Find needles in haystack. Similar in API as :func:`numpy.isin`.
+    Find test_elements in element. Similar in API as :func:`numpy.isin`.
 
-    Calculates `haystack in needles`, broadcasting over *haystack elements only*. Returns a boolean array of the same shape as *haystack* that is `True` where an element of *element* is in *needles* and `False` otherwise.
+    Calculates `element in test_elements`, broadcasting over *element elements only*. Returns a boolean array of the same shape as *element* that is `True` where an element of *element* is in *test_elements* and `False` otherwise.
 
-    This works by first transforming *needles* to an array with one more
-    dimension than the *haystack*, placing the *needles* at *axis*, and then doing a
+    This works by first transforming *test_elements* to an array with one more
+    dimension than the *element*, placing the *test_elements* at *axis*, and then doing a
     comparison.
 
     Args:
-        haystack (dak.Array or ak.Array): haystack of values.
-        needles (dak.Array or ak.Array): one-dimensional set of needles to find in haystack.
+        element (dak.Array or ak.Array): input array of values.
+        test_elements (dak.Array or ak.Array): one-dimensional set of values against which to test each value of *element*.
         axis (int): the axis along which the comparison is performed
 
     Returns:
-        dak.Array or ak.Array: result of comparison for needles in haystack
+        dak.Array or ak.Array: result of comparison for test_elements in *element*
 
     Example:
         >>> import awkward as ak
@@ -36,14 +36,14 @@ def isin(haystack: Array, needles: dak.Array | ak.Array, axis: int = -1) -> Arra
         >>> ats.isin(truth_origins, prompt_origins).to_list()
         [[True, True, False], [False], [False, False, True], [True]]
     """
-    assert needles.ndim == 1, "Needles must be one-dimensional"
+    assert test_elements.ndim == 1, "test_elements must be one-dimensional"
     assert axis >= -1, "axis must be -1 or positive-valued"
-    assert axis < haystack.ndim + 1, "axis too large for the haystack"
+    assert axis < element.ndim + 1, "axis too large for the element"
 
-    # First, build up the transformation, with slice(None) indicating where to stick the needles
-    reshaper: list[None | slice] = [None] * haystack.ndim
-    axis = haystack.ndim if axis == -1 else axis
+    # First, build up the transformation, with slice(None) indicating where to stick the test_elements
+    reshaper: list[None | slice] = [None] * element.ndim
+    axis = element.ndim if axis == -1 else axis
     reshaper.insert(axis, slice(None))
 
     # Note: reshaper needs to be a tuple for indexing purposes
-    return cast(Array, ak.any(haystack == needles[tuple(reshaper)], axis=-1))
+    return cast(Array, ak.any(element == test_elements[tuple(reshaper)], axis=-1))
